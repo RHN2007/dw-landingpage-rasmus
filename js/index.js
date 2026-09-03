3b886ad301002634b9c71c0ca7a41cecb25979be
Rasmus Nielsen <rasmushulbaknielsen@gmail.com>
2026-09-03 13:59:24 +0200

    started work on hero div

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
 // header
 let headerElement = document.querySelector(".header")
-let headerArrow = document.createElement("p")
-let headerAvatar = document.createElement("img")
 
+let headerArrow = document.createElement("p")
 headerArrow.textContent = `<`
 headerArrow.classList.add("header__arrow")
 
+let headerAvatar = document.createElement("img")
 headerAvatar.setAttribute("src", "https://placehold.co/32x32")
 headerAvatar.classList.add("header__avatar")
 
 headerElement.append(headerArrow, headerAvatar)
 
-
-
 // hero
 let heroElement = document.querySelector(".hero")
-heroElement.style.backgroundImage = `url(${hero.image})` 
+heroElement.style.backgroundImage = `url(${hero.image})`
+
+let heroDiv = document.createElement("div")
+heroDiv.classList.add("hero__div")
+
+const heroHeader = document.createElement("h1")
+heroHeader.textContent = `${hero.headline}`
+
+const heroParagraph = document.createElement("p")
+heroParagraph.textContent = `${hero.copy}`
+
+const heroButton = document.createElement("a")
+
+
+heroDiv.append(heroHeader, heroParagraph)
+heroElement.append(heroDiv)
+
 
 
 
