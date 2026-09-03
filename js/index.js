15474b32c4167f191b791d3ac82e963322e56d3b
Rasmus Nielsen <rasmushulbaknielsen@gmail.com>
2026-09-03 12:50:41 +0200

    Start på header og hero

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
-// slå dig løs her... 
+// header
+let headerElement = document.querySelector(".header")
+
+
+// hero
+let heroElement = document.querySelector(".hero")
+heroElement.style.backgroundImage = `url(${hero.image})` 
+
+
+
+
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
